cb9a84cc1b8b3be776785170c3661b52a7ba654d
igid26 <majan261166@gmail.com>
2024-05-28 12:50:07 +0200

    Pridanie podmienky pre obmedzenie zobrazenia len pre pokladňu.

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ add_filter('gettext', 'translate_woo_checkout_wpml');
 add_filter('ngettext', 'translate_woo_checkout_wpml');
 
 function translate_woo_checkout_wpml($translated) {
+
+    if (strpos($_SERVER['REQUEST_URI'], '/pokladna/') !== false OR strpos($_SERVER['REQUEST_URI'], '/zur-kasse/') !== false) { //gange your checkout slug
+
     $my_current_lang = apply_filters('wpml_current_language', NULL);
 
     // Array 
@@ -56,4 +59,6 @@ function translate_woo_checkout_wpml($translated) {
     }
 
     return $translated;
+}
+
 }
